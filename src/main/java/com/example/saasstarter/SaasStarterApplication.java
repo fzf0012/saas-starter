e145b8c63d78389021ce8be0277c15b4eefa0214
Frank Fukuchi <frank.fukuchi@gmail.com>
2025-04-28 13:12:45 -0400

    Initial commit: Setup Spring Boot backend project

---
+++ src/main/java/com/example/saasstarter/SaasStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.saasstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaasStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/saasstarter/SaasStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saasstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
